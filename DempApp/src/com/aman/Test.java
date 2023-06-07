965bf76a44c628bdc27fee8998576afea47839c4
aman-ji-gupta <gamansantosh811@gmail.com>
2023-06-07 14:03:24 +0530

    demo commit

---
+++ DempApp/src/com/aman/Test.java
@@ -0,0 +1,19 @@
+package com.aman;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello..");
+		
+		char[] ch = {'a','b','c','d'};
+		for(char e : ch) {
+			System.out.print(e + " ");
+		}
+		System.out.println();
+		String s = "Aman";
+		System.out.println(s.length());
+
+	}
+
+}
